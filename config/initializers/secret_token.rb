afeb629f975e1b02f3493ffd93ee2fbfb7d67a01
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-07-23 12:56:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplicity::Application.config.secret_token = '7b22f0f736af061b42139010ede1d8439cc820b37809f04d347c97bc6b63abcb144326c071a80bca69cee29a38af538d43fed5f7199084d66abdc1ac4cba3a3c'
