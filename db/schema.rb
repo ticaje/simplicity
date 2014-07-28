fb0ff8708fc08942b74e999d36ac43523bd82080
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-07-28 13:48:07 +0200

    Add ancestry type to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140728112003) do
+ActiveRecord::Schema.define(:version => 20140728114329) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(:version => 20140728112003) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "ancestry"
   end
+
+  add_index "categories", ["ancestry"], :name => "index_categories_on_ancestry"
 
   create_table "ratings", :force => true do |t|
     t.string   "name"
