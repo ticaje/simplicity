59f8a86a1410e2aa83a1f8b4d9d5758fdc582422
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-07-29 12:17:03 +0200

    Tests for user model running

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :active, :crypted_password, :email, :email, :salt, :username
+  attr_accessible :active, :crypted_password, :email, :salt, :username
 
   has_many :translations, dependent: :destroy
   has_many :user_requests, dependent: :destroy
   has_many :requests, through: :user_requests
   has_many :votes, dependent: :destroy
+
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_presence_of :username, :crypted_password
+  validates :email, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
+  validates :username, length: { maximum: 50 }
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = create(:user)
+  end
+
+  subject { @user }
+
+  it { should respond_to(:username) }
+  it { should respond_to(:email) }
+  it { should respond_to(:active) }
+
+  it "has a valid factory" do
+    @user.should be_valid
+  end
+
+  describe "Empty fields" do
+    it "is invalid without a username" do
+      @user.username = nil
+      @user.should_not be_valid
+    end
+
+    it "is invalid without a password" do
+      @user.crypted_password = nil
+      @user.should_not be_valid
+    end
+
+    it "it is invalid without an email" do
+      @user.email = nil
+      @user.should_not be_valid
+    end
+  end
+
+  describe "when email format is invalid" do
+    it "has an invalid email format" do
+      addresses = %w[user@foo, com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when username is longer than 50 characters" do
+    it "username is restricted to 50 characters" do
+      @user.username = "a" * 51
+      @user.should_not be_valid
+    end
+  end
+end
