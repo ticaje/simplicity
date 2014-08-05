85534ba4610d250dab3d30e01db14bec049af6e1
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-08-05 13:27:25 +0200

    Customize user related routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Simplicity::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => :account,:path_names => { :sign_up => "register", :sign_in => "login" }, :controllers => { :registrations => "registrations" }
 
   resources :articles
   # The priority is based upon order of creation:
@@ -51,7 +51,7 @@ Simplicity::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
