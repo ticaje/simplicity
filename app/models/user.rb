3ed0820f458c9e0668619db7d0f117554321f91e
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-08-13 17:23:45 +0200

    Prepare user model for DSL

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
   has_many :petitions, source: :article, through: :requests
   has_many :articles, dependent: :destroy, foreign_key: :author_id
   has_many :votes, dependent: :destroy
+  has_many :assignments
+  has_many :roles, :through => :assignments
 
   has_reputation :karma,
     :source => [
@@ -29,4 +31,9 @@ class User < ActiveRecord::Base
   validates :username, length: { maximum: 50 }, :presence => true
   validates_uniqueness_of :username
 
+  def role_symbols
+    roles.map do |role|
+      role.name.underscore.to_sym
+    end
+  end
 end
