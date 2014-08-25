90f6a01fa48708c01fe8a8bdbb6173786c2b2f69
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-08-25 12:10:29 +0200

    Add useful decorating functions in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  respond_to :html
+
   protect_from_forgery
   before_filter { |c| Authorization.current_user = c.current_user }
 
@@ -10,6 +12,16 @@ class ApplicationController < ActionController::Base
   def render_with(result_info, status_code = 200, message = "Transaction Ok")
     json_response = { :result => result_info, :status => status_code, :message => message}
     render json: json_response
+  end
+
+  protected
+
+  def respond_with_decorated(variable_name, *args)
+    respond_with(decorate_resource(variable_name), *args)
+  end
+
+  def decorate_resource(name)
+    instance_variable_set("@#{name}", instance_variable_get("@#{name}").decorate)
   end
 
 end
