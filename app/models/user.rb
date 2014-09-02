c59183c0f00fae856c83c17f55fae61f9028f705
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-09-02 13:47:49 +0200

    Add validation rules on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,9 @@ class User < ActiveRecord::Base
   has_reputation :total_translation_points,
     :source => { :reputation => :points, :of => :translations }
 
-  validates :username, length: { maximum: 50 }, :presence => true
+  validates :username, length: { maximum: 30 }
+  validates_presence_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
+  validates_presence_of :email, :password, :password_confirmation
   validates_uniqueness_of :username
 
   def role_symbols
